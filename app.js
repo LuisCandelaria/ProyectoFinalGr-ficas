b25c1e798e46738310c1162b680b7a968411c490
Luis Eugenio Candelaria Azpilcueta <luiseugenio.candelaria@gmail.com>
2020-11-11 18:42:54 -0600

    base

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ var InitDemo = function () {
 		alert('Your browser does not support WebGL');
 	}
 
-	gl.clearColor(1.0, 1.0, 1.0, 1.0); //
+	gl.clearColor(0.0, 0.0, 0.0, 1.0); //
 	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
 	//
@@ -80,38 +80,7 @@ var InitDemo = function () {
 	//
 	// Create buffer
 	//
-	var triangleVertices = 
-	[ // X, Y,       R, G, B
-		0.2, 0.4,    0.0, 1.0, 1.0,
-		-0.2, -0.2,  1.0, 1.0, 0.0,
-		0.6, -0.2,   1.0, 0.0, 1.0
-	];
-
-	var triangleVertexBufferObject = gl.createBuffer();
-	gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexBufferObject);
-	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(triangleVertices), gl.STATIC_DRAW);
-
-	var positionAttribLocation = gl.getAttribLocation(program, 'vertPosition');
-	var colorAttribLocation = gl.getAttribLocation(program, 'vertColor');
-	gl.vertexAttribPointer(
-		positionAttribLocation, // Attribute location
-		2, // Number of elements per attribute
-		gl.FLOAT, // Type of elements
-		gl.FALSE,
-		5 * Float32Array.BYTES_PER_ELEMENT, // Size of an individual vertex
-		0 // Offset from the beginning of a single vertex to this attribute
-	);
-	gl.vertexAttribPointer(
-		colorAttribLocation, // Attribute location
-		3, // Number of elements per attribute
-		gl.FLOAT, // Type of elements
-		gl.FALSE,
-		5 * Float32Array.BYTES_PER_ELEMENT, // Size of an individual vertex
-		2 * Float32Array.BYTES_PER_ELEMENT // Offset from the beginning of a single vertex to this attribute
-	);
-
-	gl.enableVertexAttribArray(positionAttribLocation);
-	gl.enableVertexAttribArray(colorAttribLocation);
+	
 
 	//
 	// Main render loop
